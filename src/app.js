444dddb96753d89c33a7e5c4a99f8a85c5e214d6
CatherineProulx <48301264+CatherineProulx@users.noreply.github.com>
2025-11-11 00:39:29 +0000

    Create functions to generate random excuses

--- src/app.js
+++ src/app.js
@@ -1,11 +1,31 @@
 import "bootstrap";
 import "./style.css";
 
+let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
+let action = ['ate', 'peed', 'crushed', 'broke'];
+let what = ['my homework', 'my phone', 'the car'];
+let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function getRandomElement(array) {
+  let index = Math.floor(Math.random() * array.length)
+  let randomElement = array[index]
+  return(randomElement)
+}
+
+function getExcuse() {
+  let randomWho = getRandomElement(who);
+  let randomAction = getRandomElement(action);
+  let randomWhat = getRandomElement(what);
+  let randomWhen = getRandomElement(when);
+
+  let excuse = randomWho + " " + randomAction + " " + randomWhat + " " + randomWhen
+
+  return(excuse)
+}
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  const excuse = document.getElementById("excuse");
+  excuse.innerHTML = getExcuse();
 };
+
